a4bf21b20c70b2002c1dbe1028d8e8a969d15c7b
fierzahaikkal <fierzahf.gdscug@gmail.com>
2024-10-06 22:52:23 +0700

    chore: add ssl mode formatting string

--- internal/configs/gorm.go
+++ internal/configs/gorm.go
@@ -9,8 +9,8 @@ import (
 )
 
 func InitDB(config *Config) *gorm.DB {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		config.DBHost, config.DBUser, config.DBPassword, config.DBName, config.DBPort)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		config.DBHost, config.DBUser, config.DBPassword, config.DBName, config.DBPort, config.DBSSLMode)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
